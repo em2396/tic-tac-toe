455a9347d848ec10480d2c5d97368e7718a22d1f
Emalee Poellot <emalee.poellot@gmail.com>
2023-09-18 16:46:49 -1000

    feat/Add variables to use for functionality later and continue playerPlays function

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 //QuerySelectors Here
 var playerOneWins = document.querySelector('#left-wins');
 var playerTwoWins = document.querySelector('#right-wins');
-var cellOne = document.querySelector('#one');
-var cellTwo = document.querySelector('#two');
-var cellThree = document.querySelector('#three');
-var cellFour = document.querySelector('#four');
-var cellFive = document.querySelector('#five');
-var cellSix = document.querySelector('#six');
-var cellSeven = document.querySelector('#seven');
-var cellEight = document.querySelector('#eight');
-var cellNine = document.querySelector('#nine');
-var whosTurn = document.querySelector('.whos-turn-is-it');
-var boardGame = document.querySelector('.board');
+var cellsOneThroughNine = {
+    cellOne: document.querySelector('#one'),
+    cellTwo: document.querySelector('#two'),
+    cellThree: document.querySelector('#three'),
+    cellFour: document.querySelector('#four'),
+    cellFive: document.querySelector('#five'),
+    cellSix: document.querySelector('#six'),
+    cellSeven: document.querySelector('#seven'),
+    cellEight: document.querySelector('#eight'),
+    cellNine: document.querySelector('#nine')
+}
+var whosTurn = document.querySelector('.whos-turn-is-it')
+var boardGame = document.querySelector('.board')
 
+//Variables Here
+var winningPossibilities = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9],
+[1, 4, 7],
+[2, 5, 8],
+[3, 6, 9],
+[1, 5, 9],
+[3, 5, 7],
+];
+var currentPlayerTurn = 'X';
 
 
 //Event Listeners Here
@@ -20,8 +34,6 @@ boardGame.addEventListener('click', playerPlays);
 
 
 //Functions Here
-
-//Create a function createPlayer which creates an object, should be created on window load, need eventListener for load
 function createPlayer(playerNumber, token) {
     return {
         id: playerNumber,
@@ -41,20 +53,14 @@ createPlayer(2, 'O');
 
 function playerPlays(event) {
     var cellClicked = event.target.closest('table');
-    if (cellClicked === cellOne) {
-        cellOne.innerHTML = '';
-        cellOne = currentPlayerTurn;
+    var cellToAddToken = Object.keys(cellsOneThroughNine);
+    for (var i = 0; i < cellToAddToken.length; i++){
+        if (cellClicked === cellToAddToken[i] && !cellClicked.value) {
+            cellToAddToken[i].innerHTML = '';
+            cellToAddToken += currentPlayerTurn;
+        }
     }
 }
 
-// win possibilities 
-[1, 2, 3]
-[4, 5, 6]
-[7, 8, 9]
-[1, 4, 7]
-[2, 5, 8]
-[3, 6, 9]
-[1, 5, 9]
-[3, 5, 7]
-var currentPlayerTurn = 'X';
+
 
